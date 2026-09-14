8aba2816a5c20ed458ef7d82d6c9543e69b8c237
Mochammad Irfan Mufarrid Ramadhan <mochammadirfanmufarridramadhan@gmail.com>
2026-09-14 09:28:07 +0700

    Connect live survey statistics API

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
   GOOGLE_FORM_URL: "https://docs.google.com/forms/d/1jqWXTjtDCG_4TaXqyqwXoZRxgyRasXh6pLkTX6hcSV0/viewform",
-  // Isi dengan URL Web app Apps Script yang berakhiran /exec setelah deployment.
-  STATS_API_URL: "PASTE_URL_APPS_SCRIPT_DI_SINI",
+  STATS_API_URL: "https://script.google.com/macros/s/AKfycbzudufWaUMYF5TwU0mdhDf6P1JDxEsJQQ0s133RATEcZ74S4ASCiQKedLSW-F-EaaS7vQ/exec",
   REFRESH_MS: 30000
 };
