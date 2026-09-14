ff8e79782500fe48e34165fdce48cceb8afaf5db
Mochammad Irfan Mufarrid Ramadhan <mochammadirfanmufarridramadhan@gmail.com>
2026-09-14 09:14:56 +0700

    Update Google Form link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GOOGLE_FORM_URL: "https://forms.gle/r3HF7A4okbH2rdPX6",
+  GOOGLE_FORM_URL: "https://docs.google.com/forms/d/1jqWXTjtDCG_4TaXqyqwXoZRxgyRasXh6pLkTX6hcSV0/viewform",
   // Isi dengan URL Web app Apps Script yang berakhiran /exec setelah deployment.
   STATS_API_URL: "PASTE_URL_APPS_SCRIPT_DI_SINI",
   REFRESH_MS: 30000
